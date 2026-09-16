db8daa6cd09d312b258d51ef6c5dbcf912a4f5de
IonutD3 <146918885+IonutD3@users.noreply.github.com>
2026-09-16 17:18:39 +0300

    Add files via upload

--- 8051/01-timer-and-external-interrupts/src/main.c
+++ 8051/01-timer-and-external-interrupts/src/main.c
@@ -1,1 +1,56 @@
+/* 01-timer-and-external-interrupts
+ *
+ */
 
+#include <reg51.h>
+#include <stdio.h>
+
+sbit LED  = P1^0;
+sbit LED1  = P1^1;
+sbit LED2  = P1^2;
+
+int counter = 0; 
+int counter1 = 0;
+int i;
+
+void delay(const unsigned x){
+unsigned int i, j;
+for(i=0;i<x;i++){
+for (j=0;j<=120;j++);}
+}
+
+void timer0_isr() interrupt 1{
+TH0 = 0xFC;
+TL0 =0x18;
+counter++;
+if (counter == 1000){
+LED = ~LED;
+counter =0;}
+}
+
+void timer1_isr() interrupt 3{
+TH1 = 0xFC;
+TL1 =0x18;
+counter1++;
+if (counter1 == 500){
+LED1 =~ LED1;
+counter1 =0;}
+}
+
+void ex0_isr() interrupt 0{
+for(i=0;i<=19;i++){
+LED2 = ~LED2;
+delay(250);}
+}
+
+void main(void){ 
+ TMOD = 0x11;      
+    TR0 = 1;             
+    TR1 = 1;     
+    IT0 = 1;
+    PT0 = 0;
+    PT1 = 1;
+    IE = 0x8b;    
+    while(1)
+    {} 
+}
